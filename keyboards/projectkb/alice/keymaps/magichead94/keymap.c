4513ccdcb2d52c71004acc266f7cf9a77ef92b49
MagicHead94 <62933480+MagicHead94@users.noreply.github.com>
2020-04-02 22:50:22 +1300

    Update keymap.c

--- keyboards/projectkb/alice/keymaps/magichead94/keymap.c
+++ keyboards/projectkb/alice/keymaps/magichead94/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN1] = LAYOUT_default(
     RGB_TOG,  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  _______,
     BL_TOGG,  _______,  _______, _______,  _______, _______, _______,          _______, _______, _______, _______, _______, KC_UP, _______, _______,
-    _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,          _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______,
+    _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,               _______, _______, _______, _______, KC_LEFT, KC_RGHT,      _______,
               RGB_RMOD, RGB_HUD,  RGB_SAD,  RGB_VAD, _______, _______,          _______, _______, _______, _______, _______, KC_DOWN, _______, _______,
               _______,          _______, _______, _______,                            _______,          _______,          RESET
   )
